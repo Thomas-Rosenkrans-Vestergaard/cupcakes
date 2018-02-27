4c92196385058dc379bfc81f5d0f0a220bcc116d
Thomas Vestergaard <Thomas-Rosenkrans-Vestergaard@users.noreply.github.com>
2018-02-27 23:56:15 +0100

    updated database

--- database/Dump20180227.sql
+++ database/Dump20180227.sql
@@ -31,8 +31,18 @@ CREATE TABLE `bottoms` (
   `price` int(10) unsigned NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `id_UNIQUE` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `bottoms`
+--
+
+LOCK TABLES `bottoms` WRITE;
+/*!40000 ALTER TABLE `bottoms` DISABLE KEYS */;
+INSERT INTO `bottoms` VALUES (1,'Bottom','Vestibulum ac odio faucibus, ornare justo at, laoreet augue. Nulla dapibus ut lacus quis posuere. Quisque suscipit libero sem, ut vulputate sem pellentesque sit amet. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.',500);
+/*!40000 ALTER TABLE `bottoms` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `orders`
@@ -60,6 +70,15 @@ CREATE TABLE `orders` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
+-- Dumping data for table `orders`
+--
+
+LOCK TABLES `orders` WRITE;
+/*!40000 ALTER TABLE `orders` DISABLE KEYS */;
+/*!40000 ALTER TABLE `orders` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `presets`
 --
 
@@ -79,8 +98,18 @@ CREATE TABLE `presets` (
   KEY `topping` (`topping`),
   CONSTRAINT `presets_ibfk_1` FOREIGN KEY (`bottom`) REFERENCES `bottoms` (`id`),
   CONSTRAINT `presets_ibfk_2` FOREIGN KEY (`topping`) REFERENCES `toppings` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `presets`
+--
+
+LOCK TABLES `presets` WRITE;
+/*!40000 ALTER TABLE `presets` DISABLE KEYS */;
+INSERT INTO `presets` VALUES (1,'Presets','Vestibulum ac odio faucibus, ornare justo at, laoreet augue. Nulla dapibus ut lacus quis posuere. Quisque suscipit libero sem, ut vulputate sem pellentesque sit amet. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.',1,1,1000);
+/*!40000 ALTER TABLE `presets` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `toppings`
@@ -96,8 +125,18 @@ CREATE TABLE `toppings` (
   `price` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `id_UNIQUE` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `toppings`
+--
+
+LOCK TABLES `toppings` WRITE;
+/*!40000 ALTER TABLE `toppings` DISABLE KEYS */;
+INSERT INTO `toppings` VALUES (1,'Topping','Vestibulum ac odio faucibus, ornare justo at, laoreet augue. Nulla dapibus ut lacus quis posuere. Quisque suscipit libero sem, ut vulputate sem pellentesque sit amet. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.',500);
+/*!40000 ALTER TABLE `toppings` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `users`
@@ -112,13 +151,23 @@ CREATE TABLE `users` (
   `email` varchar(45) NOT NULL,
   `password` varchar(60) NOT NULL,
   `balance` int(10) unsigned NOT NULL DEFAULT '0',
-  `role` TINYINT NOT NULL DEFAULT '0',
+  `role` tinyint(4) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`,`username`,`email`),
   UNIQUE KEY `id_UNIQUE` (`id`),
   UNIQUE KEY `username_UNIQUE` (`username`),
   UNIQUE KEY `email_UNIQUE` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (4,'Thomas','tvestergaard@hotmail.com','$2a$10$hYjNfeJByPpIzPQ8Hs907ONaxWOiStSMYNAdKPg2i0VEgrQ6NGLBC',0,1);
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -129,4 +178,4 @@ CREATE TABLE `users` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-02-27 16:57:39
+-- Dump completed on 2018-02-27 23:55:54
